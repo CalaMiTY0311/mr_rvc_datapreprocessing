c801def1740769dfdcc0600ee9f95b61b3f384a4
CalaMiTY0311 <axelsilas12@naver.com>
2024-01-09 02:23:10 +0900

    google trans add

--- voice_to_text.py
+++ voice_to_text.py
@@ -7,3 +7,13 @@ r = sr.Recognizer()
 mic = sr.Microphone()
 with mic as source:
     audio = r.listen(source)
+
+print(r.recognize_google(audio))
+
+import googletrans #pip
+
+translator = googletrans.Translator()
+
+answer = r.recognize_google(audio)
+result = translator.translate(answer, dest='en')
+print(result)
