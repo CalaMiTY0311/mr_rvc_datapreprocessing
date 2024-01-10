619d4a4a86e799b1d092dbb712229a628213eda9
CalaMiTY0311 <axelsilas12@naver.com>
2024-01-10 22:11:05 +0900

    dataset audio 44.1hz 16bit 15second

--- test.py
+++ test.py
@@ -13,9 +13,9 @@ for wav_file in os.listdir(input_dir):
     if wav_file.endswith('.wav'):
         wav_path = os.path.join(input_dir, wav_file)
 
-        y, sr = librosa.load(wav_path, sr=16000)
+        y, sr = librosa.load(wav_path, sr=44100, mono=True)
 
         for i, start in enumerate(range(0, len(y), interval_seconds * sr), 1):
             segment = y[start:start + interval_seconds * sr]
-            output_file = os.path.join(output_dir, f"{os.path.splitext(wav_file)[0]}_{i - 1}.wav")
-            sf.write(output_file, segment, sr)
+            output_file = os.path.join(output_dir, f"dataset_{i - 1}.wav")
+            sf.write(output_file, segment, sr, subtype='PCM_16')
